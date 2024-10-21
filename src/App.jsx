0db4095ada2d18319567aba0cef9cfb2a8ab1d05
abkgami <yahabubakar1@gmail.com>
2024-10-21 22:09:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import video from "../assets/hero.mp4";
+import logo from "../assets/logo.png";
+import hero from "../assets/hero.jpeg";
+const HeroSection = () => {
+  return (
+    <section className="relative flex h-screen items-center justify-center">
+      <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
+        <video
+          src={video}
+          autoPlay
+          loop
+          playsInline
+          muted
+          poster={hero}
+          className="h-full w-full object-cover"
+        ></video>
+      </div>
+      <div
+        className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent
+      from-70% to-black"
+      ></div>
+      <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
+        <img src={logo} alt="Tuwo Lagos" className="w-full p-4 " />
+        <p className="p-4 text-lg text-white tracking-tighter">Lagos - NG</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
