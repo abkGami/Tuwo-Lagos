b65a5ded5bf71b8c2869f09a4278d82cb183a44e
abkgami <yahabubakar1@gmail.com>
2024-10-22 10:44:01 +0100

    Dishes section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Dishes from "./components/Dishes";
 
 const App = () => {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
       <Navbar />
+      <Dishes />
     </main>
   );
 };
